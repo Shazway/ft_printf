c270a38c800622cf7c89fe658c052f74585cd3c2
tmoragli <tmoragli@student.42.fr>
2021-08-10 01:28:36 +0200

    Modif

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main(void)
 {
 	printf("\n\n");
-	printf("RETURN PRINTF : %d\n", printf("printf    :  %d\n", -2147483648));
-	printf("RETURN FT_PRINTF : %d\n", ft_printf("ft_printf :  %d\n", -2147483648));
+	printf("RETURN PRINTF : %d\n", printf("printf    :  %d\n", 0));
+	printf("RETURN FT_PRINTF : %d\n", ft_printf("ft_printf :  %d\n", 0));
 }
