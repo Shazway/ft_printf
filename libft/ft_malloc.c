43bb99f9ccf6b23a7130ae06d029ef0994180ed2
tmoragli <tmoragli@student.42.fr>
2021-08-10 16:00:05 +0200

    Modif

--- libft/ft_malloc.c
+++ libft/ft_malloc.c
@@ -6,13 +6,13 @@
 /*   By: tmoragli <tmoragli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 15:25:31 by tmoragli          #+#    #+#             */
-/*   Updated: 2021/08/10 15:42:59 by tmoragli         ###   ########.fr       */
+/*   Updated: 2021/08/10 16:00:00 by tmoragli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_malloc_array(t_size size, char *p, int count)
+int	ft_malloc_array(int size, char *p, int count)
 {
 	p = malloc(sizeof(size) * (count + 1));
 	if (!p)
