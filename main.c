35d0fbfb12c5edda8cfcbacf917249a0d5620d96
tmoragli <tmoragli@student.42.fr>
2021-08-11 18:49:39 +0200

    Some norm changes and clean up

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "ft_printf.h"
-
-int main(void)
-{
-	char *str = "s";
-	printf("\n\n");
-	printf("RETURN PRINTF : %d\n", printf("printf    :  Explosion : %d pourquoi tu %s va a cette adresse ? %p quand clairement %x", 42, "blblbl", str, 42));
-	printf("RETURN FT_PRINTF : %d\n", ft_printf("ft_printf :  Explosion : %d pourquoi tu %s va a cette adresse ? %p quand clairement %x", 42, "blblbl", str, 42));
-}
